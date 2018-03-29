dfafd987f9e77b25f24a2983025b655bb7ba0d69
Marcos <msanchez@katodia.com>
2018-03-29 12:53:10 +0200

    Separate redis-client instantiation
    Although there is no pooling from client connections since we are under a
    single-threaded app, I rather have one instance defined in a separate
    file and import it whenever required.

---
+++ app/providers/redis.js
@@ -0,0 +1,16 @@
+const config = require('../config');
+const redis = require('redis');
+const asyncRedis = require('async-redis');
+
+let client = redis.createClient({
+    'host': config.REDIS.HOST,
+    'port': config.REDIS.PORT
+});
+
+const asyncClient = asyncRedis.decorate(client);
+
+asyncClient.on("error", function (err) {
+    console.error("Error " + err);
+});
+
+module.exports = asyncClient;
