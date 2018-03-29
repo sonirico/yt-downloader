d237f277a768062b697f1294705f586bfb347cb2
Marcos <msanchez@katodia.com>
2018-03-29 12:55:38 +0200

    Add controller to manage video downloads
    AsyncFunctions are being used so as to take advantage of async/await and
    avoid callback hell. Some settings like destination folder for
    convertions are still to be defined.

---
+++ app/controllers/downloader.js
@@ -0,0 +1,48 @@
+const config = require('../config');
+const readline = require('readline');
+const ytdl     = require('ytdl-core');
+const ffmpeg   = require('fluent-ffmpeg');
+const asyncHandler = require('../utils/async');
+const redisClient = require('../providers/redis');
+
+async function getOrSetAudioById (video_id) {
+    await redisClient.select(config.REDIS.DDBB.AUDIO_FILES);
+
+    let res = await redisClient.get(video_id);
+
+    if (! res) {
+        let info = await cntrl.informer.getInfo(url);
+
+        await redisClient.set(url, JSON.stringify(info));
+
+        return info;
+    }
+
+    return JSON.parse(res);
+};
+
+async function download_to_mp3 (video_id) {
+    let stream = ytdl(video_id, {
+      quality: 'highestaudio',
+      //filter: 'audioonly',
+    });
+
+    let start = Date.now();
+    let audio_path = `/srv/${video_id}.mp3`
+
+    return new Promise((res, rej) => {
+        ffmpeg(stream)
+            .audioBitrate(128)
+            .save(audio_path)
+            .on('progress', (p) => {
+                readline.cursorTo(process.stdout, 0);
+                process.stdout.write(`${p.targetSize}kb downloaded`);
+            })
+            .on('end', () => {
+                console.log(`\ndone, thanks - ${(Date.now() - start) / 1000}s`);
+                res();
+            });
+    });
+}
+
+module.exports.download_to_mp3 = download_to_mp3;
