38aa573e8bf3588bcc68f3ebe841e171ee4d7f59
Fernando <fernando24164@gmail.com>
2018-04-22 21:04:44 +0200

    Add mocha test to webpack branch

---
+++ app/tests/app_spec.js
@@ -0,0 +1,21 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const expect = chai.expect;
+const app = require('../server/app');
+
+chai.use(chaiHttp);
+
+describe('yt-dowloader', function () {
+    describe('index path', function () {
+        it("smoke test", function () {
+            expect(true).to.be.equal(true);
+        });
+        it('should responds with code 200', function (done) {
+            chai.request(app).get('/').end(function (err, res) {
+                let status_response = res.statusCode;
+                done();
+                expect(status_response).to.be.equal(200);
+            })
+        });
+    })
+});
